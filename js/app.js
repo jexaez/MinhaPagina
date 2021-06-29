a66ad689b010d691baec4181d89bdcb03ad1d4d2
Jani Exaez <exaezsa@gmail.com>
2021-06-28 20:44:06 -0400

    Nova pagina

---
+++ js/app.js
@@ -0,0 +1,17 @@
+
+function MostrarImagen() {
+    alert("Aaaaaaaaaaaaaaa");
+}
+
+function MostrarImagen2() {
+    let resposta = confirm("Voce quer um premio?");
+
+    if (resposta){
+        alert("quer?");
+        alert("Infelizmente,Acabou");
+    }
+     else{
+        alert("não quer");
+    }
+
+}
